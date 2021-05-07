753f92bff8d1d96a1ba578cc15291f6e5a26035d
tsuji <tsukko@gmail.com>
2021-05-07 17:14:12 +0900

    f

--- organize_photos.py
+++ organize_photos.py
@@ -10,23 +10,15 @@ output_image_dir = "intermediate_img"
 input_pdf_dir = "input"
 
 
-def exchange_pdf_to_image(file_name):
+def exchange_pdf_to_image(file_path):
     """
     ファイル名の変更
 
-    :param file_name: ファイル名（パス含む）
+    :param file_path: ファイル名（パス含む）
     :return: 変換後のファイル名
     """
-    # debug
-    # 作成日時
-    ct = os.path.getctime(file_name)
-    cd = datetime.datetime.fromtimestamp(ct)
-    # 更新日時
-    mt = os.path.getmtime(file_name)
-    md = datetime.datetime.fromtimestamp(mt)
-
     # ファイル名に設定する日時の取得
-    file_date = get_date(file_name)
+    file_date = get_date(file_path)
 
     # 保存用パスの設定とフォルダ生成
     file_directory = file_date.strftime("%Y%m")
@@ -35,7 +27,7 @@ def exchange_pdf_to_image(file_name):
 
     # 保存ファイル名の設定
     new_file_name = file_date.strftime("%Y%m%d_%H%M%S")
-    file_ext = os.path.splitext(file_name)[1]
+    file_ext = os.path.splitext(file_path)[1]
     new_file_path_base = save_path + "/IMG_" + new_file_name
     new_file_path = new_file_path_base + file_ext
     # print("1: ", new_file_path)
@@ -47,29 +39,38 @@ def exchange_pdf_to_image(file_name):
         new_file_path = new_file_path_base + "_m" + str(num) + file_ext
         # print("2: ", new_file_path)
 
-    # new_path = shutil.copyfile(file_name, new_file_path)
-    new_path = shutil.move(file_name, new_file_path)
+    new_path = file_path
+    # new_path = shutil.copyfile(file_path, new_file_path)
+    # new_path = shutil.move(file_path, new_file_path)
 
-    print(file_name, new_file_name, ", file_date:", file_date, ", 作成日時：", cd, ", 更新日時：", md)
+    # debug
+    # 作成日時
+    cd = datetime.datetime.fromtimestamp(os.path.getctime(new_path))
+    # 更新日時
+    md = datetime.datetime.fromtimestamp(os.path.getmtime(new_path))
+    print("old: {0}, new: {1}, 作成日時： {2}, 更新日時： {3}".format(file_path, new_path, cd, md))
     return new_path
 
 
-def get_exif_of_image(file):
+def get_exif_of_image(file_path):
     """
     Get EXIF of an image if exists.
     指定した画像のEXIFデータを取り出す
 
-    :param file: ファイル名（パス含む）
+    :param file_path: ファイル名（パス含む）
     :return: exif_table Exif データを格納した辞書
     """
     # Exif データを取得
     # 存在しなければそのまま終了 空の辞書を返す
     try:
-        im = Image.open(file)
+        im = Image.open(file_path)
         exif = im._getexif()
     except AttributeError:
         return {}
     except UnidentifiedImageError:
+        return {}
+
+    if not exif:
         return {}
 
     # タグIDそのままでは人が読めないのでデコードして
@@ -78,29 +79,33 @@ def get_exif_of_image(file):
     for tag_id, value in exif.items():
         tag = TAGS.get(tag_id, tag_id)
         exif_table[tag] = value
+        print(tag, tag_id, value)
+        # if "DateTimeOriginal" in tag:
+        #     print(tag, tag_id, value)
+        # if tag_id == 0x9003:
+        #     print(tag, tag_id, value)
 
     return exif_table
 
 
-def get_date(file_name) -> datetime:
+def get_date(file_path):
     """
     exifに含まれている日付（取得できれば）か、
     ファイルの作成日時、更新日時のうち古い日付を返却する
 
-    :param file_name: ファイル名（パス含む）
+    :param file_path: ファイル名（パス含む）
     :return: 日付
     """
-    exif = get_exif_of_image(file_name)
+    exif = get_exif_of_image(file_path)
     if "DateTimeOriginal" in exif:
-        # strftime() で新しい名前のフォーマットを指定
-        # print("debug, a:", exif["Date\\\\\\TimeOriginal"])
+        print("debug, a:", exif["DateTimeOriginal"])
         return datetime.datetime.strptime(exif["DateTimeOriginal"], "%Y:%m:%d %H:%M:%S")
     else:
         # 作成日時
-        ct = os.path.getctime(file_name)
+        ct = os.path.getctime(file_path)
         # 更新日時
-        mt = os.path.getmtime(file_name)
-        # print("debug, b: ", datetime.datetime.fromtimestamp(min(ct, mt)))
+        mt = os.path.getmtime(file_path)
+        print("debug, b: ", datetime.datetime.fromtimestamp(min(ct, mt)))
         return datetime.datetime.fromtimestamp(min(ct, mt))
 
 
@@ -108,8 +113,8 @@ def run():
     os.makedirs(output_image_dir, exist_ok=True)
     # jpg_list = [p for p in glob.glob(input_pdf_dir + "/**", recursive=True) if re.search('/*\.(jpg|JPG|jpeg|png|gif|bmp)', str(p))]
     jpg_list = glob.glob(os.path.abspath(input_pdf_dir) + "/**/*.*", recursive=True)
-    for file_name in jpg_list:
-        exchange_pdf_to_image(file_name)
+    for file_path in jpg_list:
+        exchange_pdf_to_image(file_path)
 
 
 run()
